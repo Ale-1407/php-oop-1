b37cbf25759e4963e7cdb3d0a956980ee0415b4b
Ale-1407 <alessiacarrera07@gmail.com>
2022-12-17 11:36:05 +0100

    attributi  istanze

--- index.php
+++ index.php
@@ -8,8 +8,16 @@ class Movie {
 }
 
 $movie1 = new Movie();
+$movie1 -> nome = "Avatar";
+$movie1 -> anno = "2009";
+
 $movie2 = new Movie();
+$movie1 -> nome = "Guardiani della Galassia";
+$movie1 -> anno = "2017";
+
 $movie3 = new Movie();
+$movie1 -> nome = "Doctor Strange";
+$movie1 -> anno = "2016";
 
 
 ?>
