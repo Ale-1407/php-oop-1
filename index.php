f347c5626a2cc807c3e80b2fc81e5fbf509039bd
Ale-1407 <alessiacarrera07@gmail.com>
2022-12-17 11:19:19 +0100

    classe e sue istanze

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once __DIR__ . '/classi/classi.php';
+
+class Movie {
+    public $nome;
+    public $anno;
+}
+
+$movie1 = new Movie();
+$movie2 = new Movie();
+$movie3 = new Movie();
+
+
+?>
