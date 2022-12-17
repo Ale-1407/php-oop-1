087f8726d5a2eeb9245588c0b6a1fe58cd3ab12b
Ale-1407 <alessiacarrera07@gmail.com>
2022-12-17 11:56:03 +0100

    modifiche codice

---
+++ classi/classi.php
@@ -0,0 +1,8 @@
+<?php
+
+class Movie {
+    public $nome;
+    public $anno;
+}
+
+?>
--- index.php
+++ index.php
@@ -2,22 +2,18 @@
 
 include_once __DIR__ . '/classi/classi.php';
 
-class Movie {
-    public $nome;
-    public $anno;
-}
 
 $movie1 = new Movie();
 $movie1 -> nome = "Avatar";
-$movie1 -> anno = "2009";
+$movie1 -> anno = 2009;
 
 $movie2 = new Movie();
 $movie2 -> nome = "Guardiani della Galassia";
-$movie2 -> anno = "2017";
+$movie2 -> anno = 2017;
 
 $movie3 = new Movie();
 $movie3 -> nome = "Doctor Strange";
-$movie3 -> anno = "2016";
+$movie3 -> anno = 2016;
 
 
 ?>
