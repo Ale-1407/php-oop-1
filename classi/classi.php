58d6964be24e501c7ff1745ca2866ca691f4afcf
Ale-1407 <alessiacarrera07@gmail.com>
2022-12-19 09:51:03 +0100

    costruttore e metodo

--- classi/classi.php
+++ classi/classi.php
@@ -3,6 +3,24 @@
 class Movie {
     public $nome;
     public $anno;
+    public $age;
+
+    public function __construct( $_nome, $_anno, $_age ){
+     $this -> nome = $_nome; 
+     $this -> anno = $_anno; 
+     $this -> age = $_age; 
+    }
+
+    public function perTutti(){
+        if( $this -> age > 18  ){
+            return "La visione del film è riservata ad un pubblico di soli adulti";
+        } else {
+            return "La visione del film è stata autorizzata";
+        }
+    }
+
 }
+
+
 
 ?>
--- index.php
+++ index.php
@@ -6,14 +6,18 @@ include_once __DIR__ . '/classi/classi.php';
 $movie1 = new Movie();
 $movie1 -> nome = "Avatar";
 $movie1 -> anno = 2009;
+$movie1 -> age = 20;
 
 $movie2 = new Movie();
 $movie2 -> nome = "Guardiani della Galassia";
 $movie2 -> anno = 2017;
+$movie2 -> age = 18;
+
 
 $movie3 = new Movie();
 $movie3 -> nome = "Doctor Strange";
 $movie3 -> anno = 2016;
+$movie3 -> age = 10;
 
 
 ?>
